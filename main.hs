81c688017811816cfd185084cf8ca1da845a6049
nofater <nofater@gmail.com>
2016-01-08 19:09:58 +0300

    Parsing - Basic lisp literals

--- main.hs
+++ main.hs
@@ -1,17 +1,66 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
 
+-- data ParsecT s u m a
+-- s - stream type
+-- u - user state type
+-- m - underlying monad 
+-- a - return type
+--
+-- type Parsec s u = ParsecT s u Identity
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+             deriving Show
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value: " ++ show val
+readExpr input =
+  case parse parseExpr "source" input of
+    Left err -> "No match: " ++ show err
+    Right val -> "Found value: " ++ show val 
+
 
 main :: IO ()
 main = do
   (expr:_) <- getArgs
-  putStrLn (readExpr expr)
+  putStrLn $ "Source: " ++ expr
+  putStrLn $ readExpr expr
 
