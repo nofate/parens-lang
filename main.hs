61f1c1241433f8d40d7d439ba5b6f5aa0f693c98
nofater <nofater@gmail.com>
2016-01-08 18:21:01 +0300

    Done "First_Steps"

--- main.hs
+++ main.hs
@@ -1,1 +1,12 @@
-git 
+module Main where
+import System.Environment
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let a = read $ args !! 0
+  let b = read $ args !! 1
+  let c = a + b
+  putStrLn ("Sum: " ++ show a ++ " + " ++ show b ++ " = " ++ show c)
+  name <- getLine
+  putStrLn ("Hello " ++ name)
