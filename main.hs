06f1c53046402efce05ecd5bb0d98737c5e0379f
nofater <nofater@gmail.com>
2016-01-23 13:31:31 +0300

    Eval - Basic arithmetics

--- main.hs
+++ main.hs
@@ -35,6 +35,8 @@ showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
 showVal (Char c) = "#\\" ++ [c]
 
+-- Parsing
+
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
@@ -119,17 +121,54 @@ parseExpr = parseAtom
                char ')'
                return x 
 
-readExpr :: String -> String
+-- Evaluation
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = 
+  let parsed = reads n :: [(Integer, String)] in
+    if null parsed
+    then 0
+    else fst $ parsed !! 0  
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("*", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = 
+  maybe (Bool False) ($ args) $ lookup func primitives
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval val@(Char _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+
+-- Input
+
+readExpr :: String -> LispVal
 readExpr input =
   case parse parseExpr "source" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value: " ++ show val 
-
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val 
 
 
 main :: IO ()
 main = do
   expr <- getLine
-  putStrLn $ "Source: " ++ expr
-  putStrLn $ readExpr expr
+  print $ eval $ readExpr expr
 
